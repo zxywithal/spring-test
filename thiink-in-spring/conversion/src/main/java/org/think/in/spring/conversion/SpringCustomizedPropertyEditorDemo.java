3abc7d044a4b485531a625d7d654be09636952c8
zxy <zxywithal@163.com>
2022-05-10 16:47:08 +0800

    ConditionalGenericConverter 示例

--- thiink-in-spring/conversion/src/main/java/org/think/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
+++ thiink-in-spring/conversion/src/main/java/org/think/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
@@ -11,6 +11,13 @@ public class SpringCustomizedPropertyEditorDemo {
     public static void main(String[] args) {
         //创建beanFactory 容器
         ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/property-editors-context.xml");
+        //AbstractApplicationContext -> "conversionService" ConversionService Bean
+        //->ConfigurableBeanFactory#setConversionService(conversionService)
+        //->AbstractAutowireCapableBeanFactory#instantiateBean
+        //->AbstractBeanFactory#getConversionService->
+        // BeanDefinition ->BeanWrapper -> 属性转换（数据来源：PropertyValues）
+        //setPropertyValues（PropertyValues）->TypeConverter#convertIfNessary
+        //TypeConverterDelegate#convertIfNessary->PropertyEditor or ConversionService
         User user = applicationContext.getBean("user", User.class);
         System.out.println(user);
 
