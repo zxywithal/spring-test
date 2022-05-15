d845010c0f36b226bdd10e4259019a757f26fc40
zxy <zxywithal@163.com>
2022-05-15 10:51:50 +0800

    profile示例

---
+++ thiink-in-spring/annotation/src/main/java/org/think/in/spring/annotation/profile/ProfileDemo.java
@@ -0,0 +1,48 @@
+package org.think.in.spring.annotation.profile;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+/**
+ * {@link Profile}
+ * @see Environment#getActiveProfiles()
+ */
+@Configuration
+public class ProfileDemo {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(ProfileDemo.class);
+        ConfigurableEnvironment environment = ctx.getEnvironment();
+        //设置默认的profile 为odd，被激活的profile为even
+        //被激活优先级大于默认，
+        environment.setDefaultProfiles("odd");
+        environment.setActiveProfiles("even");
+        ctx.refresh();
+        Integer number = ctx.getBean("number", Integer.class);
+        System.out.println(number);
+        ctx.close();
+    }
+
+    /**
+     * 两个bean的名字设置为相同"number"
+     * 但是profile 一个为odd 另一个为even
+     * spring 容器中必须有激活或者默认的profile，否则找不到对应的bean
+     * @return
+     */
+    @Bean(name = "number")
+    @Profile("odd") //奇数
+    public Integer odd(){
+        return 1;
+    }
+
+    @Bean(name = "number")
+    @Profile("even") //偶数
+    public Integer even(){
+        return 2;
+    }
+}
